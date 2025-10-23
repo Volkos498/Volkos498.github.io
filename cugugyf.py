83fc81dba4d112920ceb992d348527e485283179
Your Name <you@example.com>
2025-10-23 17:06:26 +0300

    uhbh

--- cugugyf.py
+++
@@ -1,3 +0,0 @@
-mhgo = {"key": 1285463, "User": "@.ai"}
-print(mhgo["key"])
-print(mhgo["User"])
--- k.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-data = requests.get("https://randomuser.me/api/?result=1").json()
-print(type(data['results']))
